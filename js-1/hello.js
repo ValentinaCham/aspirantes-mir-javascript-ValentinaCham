7d9b597e8eaf2f484985a2e2631cad564ebc0195
ValentinaCham <vchambilllap@unsa.edu.pe>
2023-05-10 19:41:47 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,10 @@
+// escribe la función hola acá
+
+function hola(str){
+  return "Hola "+ str + "!"
+} 
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
